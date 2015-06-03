6691660abb65f73e219e525b2ed0a90c1caf91be
alienjun <alienjun@126.com>
2015-06-03 23:06:56 +0800

    init

---
+++ CITwirlDistortionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CITwirlDistortionDemo
+//
+//  Created by AlienJunX on 15/6/3.
+//  Copyright (c) 2015年 com.alienjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
